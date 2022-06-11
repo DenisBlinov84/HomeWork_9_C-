89a8f7f85dd91b72c9b57c530ff89b8766804f79
Denis <blinovda1@mail.ru>
2022-06-11 16:43:52 +0300

    add task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Задайте значения M и N. Напишите программу, которая найдёт сумму
+//    натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int AllNumersSum(int M, int N)
+{
+    if (M > N) return 0;    
+    return AllNumersSum(M, N - 1) + N;
+}
+
+Console.WriteLine(AllNumersSum(3, 21));
