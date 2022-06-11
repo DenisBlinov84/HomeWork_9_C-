7865b2abb066aaf41cac576c94b7edc7d01cf594
Denis <blinovda1@mail.ru>
2022-06-11 16:40:06 +0300

    add task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Задайте значения M и N. Напишите программу, которая выведет все
+//    чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+
+void AllEvenNumers(int M, int N)
+{
+    if (M > N) return;
+    if (M % 2 == 1) M += 1;
+
+    Console.Write($"{M}, ");
+    AllEvenNumers(M + 2, N);    
+}
+
+AllEvenNumers(3, 10);
